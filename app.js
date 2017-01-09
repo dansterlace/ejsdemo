27a70fa96ef4336e172e985758a458393f02d32c
Dan <dpsterlace@gmail.com>
2017-01-09 13:02:31 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var app = express();
+
+app.get("/", function(req,res){
+    res.render("home.ejs");
+});
+
+app.get("/fallinlovewith/:thing", function(req, res){
+    var thing = req.params.thing;
+    res.render("love.ejs", {thingVar: thing});
+});
+
+app.get("/posts", function(req, res){
+    var posts = [
+        {title: "Post 1", author: "Dan"},
+        {title: "Post 2", author: "Alex"},
+        {title: "Post 3", author: "Katie"}
+    ];
+    res.render("posts.ejs", {posts: posts});
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Server is listening!");
+});
