6fdadc2006b55d02a8b17e4599972fcd207bb15f
Dan <dpsterlace@gmail.com>
2017-01-09 14:42:28 +0000

    new template flow

--- app.js
+++ app.js
@@ -5,21 +5,11 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 app.get("/", function(req,res){
-    res.render("home.ejs");
+    res.render("home");
 });
 
-app.get("/fallinlovewith/:thing", function(req, res){
-    var thing = req.params.thing;
-    res.render("love.ejs", {thingVar: thing});
-});
-
-app.get("/posts", function(req, res){
-    var posts = [
-        {title: "Post 1", author: "Dan"},
-        {title: "Post 2", author: "Alex"},
-        {title: "Post 3", author: "Katie"}
-    ];
-    res.render("posts.ejs", {posts: posts});
+app.get("/friends", function(req, res){
+    res.render("friends");
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
